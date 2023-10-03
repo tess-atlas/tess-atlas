eeb4e51fe231acf2449964ea10e637d9bfa02a1a
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-10-03 21:35:56 +1300

    add pytest badge

--- src/tess_atlas/__init__.py
+++ src/tess_atlas/__init__.py
@@ -8,7 +8,7 @@ __email__ = "foreman.mackey@gmail.com"
 __uri__ = "https://github.com/dfm/tess-atlas"
 __license__ = "MIT"
 __description__ = "All the TESS exoplanets!"
-__copyright__ = "Copyright 2020 TESS Atlas developers"
+__copyright__ = "Copyright 2020-2023 TESS Atlas developers"
 __contributors__ = "https://github.com/dfm/tess-atlas/graphs/contributors"
 __website__ = "http://catalog.tess-atlas.cloud.edu.au"
 
